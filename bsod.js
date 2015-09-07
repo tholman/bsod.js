b079bd4f1c7f83a617c21f5cee00a31b6d6e4b8a
Tim <timothy.w.holman@gmail.com>
2015-09-07 19:14:44 -0400

    detect the specific browser, for the header

--- bsod.js
+++ bsod.js
@@ -11,10 +11,31 @@
      * Utils
      */
     function applyProperties( target, properties ) {
-
       for( var key in properties ) {
         target.style[ key ] = properties[ key ];
       }
+    }
+
+    // Lord knows why this is so hard.
+    // http://stackoverflow.com/questions/2400935/browser-detection-in-javascript
+    function getBrowser() {
+        var ua = navigator.userAgent, tem,
+        M = ua.match(/(opera|chrome|safari|firefox|msie|trident(?=\/))\/?\s*(\d+)/i) || [];
+
+        if(/trident/i.test(M[1])){
+            tem = /\brv[ :]+(\d+)/g.exec(ua) || [];
+            return 'IE '+(tem[1] || '');
+        }
+
+        if(M[1]=== 'Chrome'){
+            tem = ua.match(/\b(OPR|Edge)\/(\d+)/);
+            if(tem != null) return tem.slice(1).join(' ').replace('OPR', 'Opera');
+        }
+
+        M= M[2]? [M[1], M[2]]: [navigator.appName, navigator.appVersion, '-?'];
+        if((tem= ua.match(/version\/(\d+)/i))!= null) M.splice(1, 1, tem[1]);
+
+        return M[0];
     }
 
     /**
@@ -119,7 +140,7 @@
 
         var headerContent = document.createElement('span');
         applyProperties( headerContent, headerContentStyles );
-        headerContent.innerHTML = 'Chrome'; // @TODO: Browser sniffing here.
+        headerContent.innerHTML = getBrowser();
         header.appendChild( headerContent );
 
         /**
